43457ce76a13aff65a7bc083d9e60ab61e27b3ba
keerthana <pkeerthanajd@gmail.com>
2025-06-04 12:42:12 +0530

    basic ArrayList prg

---
+++ basic.java
@@ -0,0 +1,18 @@
+import java.util.*; //An example for ArrayList using Integer
+
+public class basic 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        ArrayList<Integer> nums = new ArrayList<>(); // *Resizable array * Fast access (get()) * Slower insert/delete in middle
+        for (int i = 0; i < 4; i++) 
+        {
+            System.out.print("Enter the roll no: ");
+            nums.add(s.nextInt());
+        }
+        System.out.println(nums);
+        System.out.println(nums.get(2));
+
+    }
+}
